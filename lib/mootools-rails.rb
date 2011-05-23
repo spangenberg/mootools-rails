33ffa38ee08ca72992d7b0a81064e30e9ea1b034
Pavel Pravosud <rwz@duckroll.ru>
2011-05-23 16:09:48 +0700

    updated gem to be rails 3.1 compatible

--- lib/mootools-rails.rb
+++ lib/mootools-rails.rb
@@ -1,15 +1,23 @@
 module Mootools
+  
+  PROTOTYPE_JS = %w{prototype effects dragdrop controls}
+  
   module Rails
+    
     class Railtie < ::Rails::Railtie
-
-      config.before_configuration do
-        require "mootools-rails/assert_select_mootools" if ::Rails.env.test?
-
-        jq_defaults = ::Rails.env.production? ? %w(mootools.min) : %w(mootools)
-
-        config.action_view.javascript_expansions[:defaults] = jq_defaults + %w(rails)
+      config..before_configuration do
+        if ::Rails.root.join('public/javascripts/mootools-more.min.js').exist?
+          mootools_defaults = %w(mootools mootools-more)
+          mootools_defaults.map!{|a| a + '.min'} if ::Rails.env.production?
+        else
+          mootools_defaults = ::Rails.env.production?? %w(mootools.min) : %w(mootools)
+        end
+        
+        config.action_view.javascript_expansion[:defaults] -= PROTOTYPE_JS + %w(rails)
+        config.action_view.javascript_expansion[:defaults] |= mootools_defaults
+        config.action_view.javascript_expansion[:defaults] << 'mootools_ujs'
       end
-
     end
-  end
-end
+    
+  end if ::Rails.version < '3.1'
+end 
