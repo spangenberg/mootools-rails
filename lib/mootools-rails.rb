5c3f9194f645456c7d957a5ba061af5375b600d8
Pavel Pravosud <rwz@duckroll.ru>
2011-05-23 17:17:57 +0700

    added empty rails engine

--- lib/mootools-rails.rb
+++ lib/mootools-rails.rb
@@ -1,23 +1,12 @@
 module Mootools
-  
   PROTOTYPE_JS = %w{prototype effects dragdrop controls}
-  
+
   module Rails
-    
-    class Railtie < ::Rails::Railtie
-      config..before_configuration do
-        if ::Rails.root.join('public/javascripts/mootools-more.min.js').exist?
-          mootools_defaults = %w(mootools mootools-more)
-          mootools_defaults.map!{|a| a + '.min'} if ::Rails.env.production?
-        else
-          mootools_defaults = ::Rails.env.production?? %w(mootools.min) : %w(mootools)
-        end
-        
-        config.action_view.javascript_expansion[:defaults] -= PROTOTYPE_JS + %w(rails)
-        config.action_view.javascript_expansion[:defaults] |= mootools_defaults
-        config.action_view.javascript_expansion[:defaults] << 'mootools_ujs'
-      end
+    if ::Rails.version < '3.1'
+      require 'mootools-rails/railtie'
+    else
+      require 'mootools-rails/engine'
     end
-    
-  end if ::Rails.version < '3.1'
-end 
+  end
+
+end
---
+++ lib/mootools-rails/engine.rb
@@ -0,0 +1,9 @@
+module Mootools
+  module Rails
+
+    class Engine < ::Rails::Engine
+      # just a empty engine to ensure our vendors will be added to rails assets pipe
+    end
+
+  end
+end
