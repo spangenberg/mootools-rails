3ba4d33665cc77d458f5b72226e3c0574466f91c
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-11-21 19:50:15 +0100

    add compad versions version bump

--- lib/mootools/rails/version.rb
+++ lib/mootools/rails/version.rb
@@ -1,6 +1,6 @@
 module Mootools
   module Rails
-    VERSION = "1.0.0"
+    VERSION = "1.0.1"
     MOOTOOLS_VERSION = "1.4.1"
     MOOTOOLS_MORE_VERSION  = "1.4.0.1"
     MOOTOOLS_UJS_VERSION = "2c1e98715bc0fc74afa3c79e5d125e55450ba74a"
