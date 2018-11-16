187d26586a0b719d2c9542f0e43e0bab7a20ab2d
Daniel Spangenberg <daniel@spangenberg.io>
2018-11-16 23:02:16 +0100

    Version bump

--- lib/mootools/rails/version.rb
+++ lib/mootools/rails/version.rb
@@ -1,6 +1,6 @@
 module Mootools
   module Rails
-    VERSION = '3.1.0'
+    VERSION = '3.2.0'
     MOOTOOLS_VERSION = '1.5.0'
     MOOTOOLS_MORE_VERSION  = '1.5.0'
     MOOTOOLS_UJS_VERSION = '44c5b14455b2104e79306e6d05cd478777a68c1a'
